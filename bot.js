bcaea256c36015e2ff04c329a2ba2a68941c1f9a
HarryDerbyshire <harry@derbyshire.uk>
2020-12-18 14:40:34 +0000

    Initial Commit

---
+++ bot.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+// require the discord.js module
+const Discord = require('discord.js');
+const auth = require('./auth.json');
+const config = require('./config.json');
+const { time, timeStamp } = require('console');
+const { cooldown } = require('./commands/ping');
+const prefix = config.prefix;
+
+// create a new Discord client
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync(`./commands`).filter(file => file.endsWith(`.js`));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// login to Discord with your app's token
+client.login(auth.token);
+
+client.on('message', message => {
+
+    if (message.content.includes("u7g")) {
+        message.channel.bulkDelete(1);
+    }
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return;
+
+    if (command.guildOnly && message.channel.type === 'dm') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }
+
+    if (command.args && !args.length){
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+
+});
